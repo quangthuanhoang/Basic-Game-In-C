328c9e099165e80368324c704d85fcc48ffb5ba8
thuan952 <hoangquangthuan952@gmail.com>
2020-11-17 09:30:23 +0700

    Update

--- Project 1.cpp
+++ Project 1.cpp
@@ -1,116 +1,95 @@
 #include <stdio.h>
 #include <ctime>
 #include <graphics.h>
-#define ROW 6
+
+int timePlay = 30000 ;
 char s[100];
-int sizeAlphabet = ROW*ROW;
-int timePlay = 20000 ;
+int sizeAlphabet = 36;
 int trueClicked = 0;
 int falseClicked = 0;
 int score = 0;
+int x=0,y=0;
 
-struct alphabet
-{
+struct alphabet {
     int x;
     int y;
     char text;
     bool isSpecial;
-    void draw ()
-    {
+    void draw () {
         char s[100];
-        if (isSpecial)
-        {
+        if (isSpecial) {
             settextstyle(9, 0, 1);
             setcolor(12);
             sprintf (s," %c   ",text);
-            //rectangle (x-15,y-10,x+25,y+25);
             outtextxy (x,y,s);
         }
-        else
-        {
+        else {
             settextstyle(9, 0, 1);
             setcolor(15);
             sprintf (s," %c   ",text);
-            //rectangle (x-15,y-10,x+25,y+25);
             outtextxy (x,y,s);
         }
     }
-    bool checkMouse (int xMouse,int yMouse,int widthX=30,int widthY=30)
-    {
-        if (xMouse > (x-widthX) && xMouse < (x+widthX))
-            if (yMouse > (y-widthY) && yMouse < (y+widthY))
-                return 1;
-        return 0;
-    }
+    bool checkMouse (int xMouse,int yMouse) {
+		if (xMouse > (x-30) && xMouse < (x+30))
+		if (yMouse > (y-30) && yMouse < (y+30))
+			return true;
+		return false;
+	}
 };
 
-alphabet arrayAlphabet[1000];
+alphabet arrayAlphabet[36];
 
-void createAlphabets(bool isShuffle,int xStart = 250, int yStart = 150, int widthX = 60,int widthY = 60)
-{
+void createAlphabets(bool isShuffle,int xStart = 250, int yStart = 150,
+                     int width = 60) {
     int xRandom = rand() % sizeAlphabet;
     int xOriginal = xStart ;
-    for (int i = 0 ; i < sizeAlphabet ; i++)
-    {
+    for (int i = 0 ; i < sizeAlphabet ; i++) {
         arrayAlphabet[i].x = xStart;
         arrayAlphabet[i].y = yStart;
-        if(i==xRandom)
-        {
-            arrayAlphabet[i].isSpecial = 1;
-        }
-        else
-            arrayAlphabet[i].isSpecial = 0;
-        xStart += widthX;
-        if((i+1) % ROW == 0)
-        {
-            yStart += widthY;
+        xStart += width;
+        if((i+1) % 6 == 0){
+            yStart += width;
             xStart = xOriginal;
         }
         if(isShuffle)
             arrayAlphabet[i].text = 'A' + rand() % 26 ;
+        if(i==xRandom) {
+            arrayAlphabet[i].isSpecial = true;
+        }
+        else
+            arrayAlphabet[i].isSpecial = false;
     }
 }
 void drawAlphabet()
 {
-    for(int i = 0 ; i < sizeAlphabet ; i++)
-    {
+    for(int i = 0 ; i < sizeAlphabet ; i++) {
         arrayAlphabet[i].draw();
     }
 }
-int findObjectClick(int x,int y)
-{
-    bool isCheck = 0 ;
-    for(int i = 0 ; i < sizeAlphabet ; i++)
-    {
-        isCheck = arrayAlphabet[i].checkMouse(x,y);
-        if(isCheck)
+int findObjectClick(int x,int y) {
+    for(int i = 0 ; i < sizeAlphabet ; i++) {
+        if(arrayAlphabet[i].checkMouse(x,y))
             return i;
     }
     return -1;
 }
-void check(int x,int y)
-{
+void check(int x,int y) {
     int indexObjectClick = findObjectClick(x,y);
-    bool isWin = 0;
-    isWin = arrayAlphabet[indexObjectClick].isSpecial;
-    if(isWin == 1)
-    {
+    if(arrayAlphabet[indexObjectClick].isSpecial == true) {
         createAlphabets(true);
         drawAlphabet();
         score +=5;
         trueClicked ++;
     }
-    else
-    {
+    else {
         createAlphabets(false);
         drawAlphabet();
         falseClicked ++;
     }
 }
-void process(int x,int y)
-{
-    for (;; delay(100))
-    {
+void process() {
+    for (;; delay(100)) {
         timePlay -=100;
         setcolor(14);
         settextstyle(8, 0, 1);
@@ -125,38 +104,34 @@ void process(int x,int y)
         outtextxy (50,90,s);
         sprintf (s,"Score : %d",score);
         outtextxy (50,110,s);
-        if (timePlay > 0 )
-        {
-            if (ismouseclick(WM_LBUTTONDOWN))
-            {
+        if (timePlay > 0 ) {
+            if (ismouseclick(WM_LBUTTONDOWN)){
                 getmouseclick(WM_LBUTTONDOWN, x, y);
                 check (x,y);
             }
         }
-        else
-        {
+        else {
             cleardevice();
             setcolor(15);
             settextstyle(6, 0, 3);
-            outtextxy (300,230,"GAME OVER !");
+            outtextxy (280,230,"GAME OVER !");
             settextstyle(10, 0, 2);
             sprintf (s,"Your score is : %d",score);
-            outtextxy (305,270,s);
-            getch();
+            outtextxy (290,270,s);
+            break;
         }
     }
 }
-int main ()
-{
+int main () {
     initwindow(800, 600);
     settextstyle(6, 0, 2);
     setcolor(3);
     outtextxy (220,30,"WELLCOME TO THE GAME");
     srand(time( 0));
-    int x=0,y=0;
     createAlphabets(true);
     drawAlphabet();
-    process(x,y);
+    process();
+    getch();
     closegraph();
 
 }
